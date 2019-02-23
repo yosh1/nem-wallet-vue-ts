ed71c40696f031851e3e955eccb14f8e047fbd4b
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-02-24 01:33:07 +0900

    [Update] First commit.

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,17 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import './registerServiceWorker';
+import Vuetify from 'vuetify';
+import colors from 'vuetify/es5/util/colors';
+import Toast from 'vue2-toast';
+import VueQriously from 'vue-qriously';
+
+Vue.use(Vuetify, {
+  theme: {
+    original
+  }
+})
 
 Vue.config.productionTip = false;
 
